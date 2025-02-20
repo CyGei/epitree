b2b208a8516591fb62eda2bd3173d4fd95c30c78
CyGei <c.geismar21@imperial.ac.uk>
2025-02-20 00:10:59 +0000

    added tree functions + tests + pkgdown

---
+++ R/make_tree.R
@@ -0,0 +1,116 @@
+#' Generate a Transmission Tree
+#'
+#' Creates a transmission tree with a specified number of cases and branches per case.
+#' The tree can be generated with fixed or Poisson-distributed branching factors.
+#'
+#' @param num_cases Integer. The total number of cases (nodes) in the tree.
+#' @param R Integer. The fixed number of branches per case when \code{stochastic} is \code{FALSE},
+#'           or the mean of the Poisson distribution when \code{stochastic} is \code{TRUE}.
+#' @param stochastic Logical. If \code{TRUE}, the number of branches per case is sampled from
+#'                   a Poisson distribution with mean \code{R}. Default is \code{FALSE}.
+#' @param plot Logical. If \code{TRUE}, the function will plot the generated tree. Default is \code{FALSE}.
+#' @return An igraph object representing the transmission tree.
+#' @importFrom igraph make_graph layout_as_tree V as_long_data_frame
+#' @importFrom stats rpois
+#' @examples
+#' # Generate a deterministic transmission tree
+#' deterministic_tree <- make_tree(num_cases = 15, R = 2, stochastic = FALSE, plot = TRUE)
+#'
+#' # Generate a stochastic transmission tree
+#' random_tree <- make_tree(num_cases = 15, R = 2, stochastic = TRUE, plot = TRUE)
+#' @export
+
+make_tree <- function(num_cases, R = 2, stochastic = FALSE, plot = FALSE) {
+  edges <- c()
+  current_nodes <- 1
+  next_node <- 2
+
+  while(next_node <= num_cases) {
+    new_nodes <- c()
+    for(node in current_nodes) {
+      # Determine the branching factor
+      if(stochastic) {
+        R_current <- stats::rpois(1, lambda = R)
+      } else {
+        R_current <- R
+      }
+
+      for(i in 1:R_current) {
+        if(next_node > num_cases) {
+          break
+        }
+        # Add an edge from the current node to the next node
+        edges <- c(edges, node, next_node)
+        new_nodes <- c(new_nodes, next_node)
+        next_node <- next_node + 1
+      }
+    }
+    current_nodes <- new_nodes
+    if(length(current_nodes) == 0) {
+      break
+    }
+  }
+
+  # Create the graph object
+  g <- igraph::make_graph(edges, directed = TRUE)
+
+  validate_tree(igraph::as_long_data_frame(g))
+
+  # Plot the graph if requested
+  if(plot) {
+    plot(
+      g,
+      vertex.label = igraph::V(g)$name,
+      layout = igraph::layout_as_tree,
+      vertex.color = ifelse(stochastic, "lightgreen", "skyblue"),
+      edge.arrow.size = 0.5,
+      main = ifelse(stochastic, "Randomised Transmission Tree", "Deterministic Transmission Tree")
+    )
+  }
+
+  return(g)
+}
+
+
+
+#' Shuffle Node IDs in a Graph
+#'
+#' Randomly shuffles the IDs of the nodes in a given graph and optionally plots the shuffled graph.
+#'
+#' @param g An igraph object representing the graph.
+#' @param plot Logical. If \code{TRUE}, the function will plot the shuffled graph. Default is \code{FALSE}.
+#' @return An igraph object with shuffled node IDs.
+#' @importFrom igraph vcount permute V layout_as_tree
+#' @examples
+#' # Create an example graph
+#' g <- make_tree(num_cases = 10, R = 2)
+#'
+#' # Shuffle the node IDs
+#' shuffled_graph <- shuffle_graph_ids(g, plot = TRUE)
+#' @export
+shuffle_graph_ids <- function(g, plot = FALSE) {
+  # Get the number of vertices in the graph
+  num_vertices <- igraph::vcount(g)
+
+  # Generate a random permutation of vertex IDs
+  permutation <- sample(1:num_vertices, num_vertices)
+
+  # Apply the permutation to the graph
+  g_shuffled <- igraph::permute(g, permutation)
+
+  # Update vertex labels to reflect the new IDs
+  V(g_shuffled)$name <- as.character(1:num_vertices)
+
+  if(plot){
+    plot(
+      g_shuffled,
+      vertex.label = igraph::V(g_shuffled)$name,
+      layout = igraph::layout_as_tree,
+      vertex.color = "orange",
+      edge.arrow.size = 0.5,
+      main = "Shuffled Transmission Tree"
+    )
+  }
+
+  return(g_shuffled)
+}
--- README.md
+++ README.md
@@ -1,2 +1,74 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
 # epitree
-Compare posterior sets of transmission trees.
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The `epitree` package provides a statistical framework for comparing
+posterior distributions of transmission trees (*and soon*, phylogenetic
+trees) generated through Bayesian inference. It employs PERMANOVA (via
+[`vegan::adonis2`](https://vegandevs.github.io/vegan/reference/adonis.html))
+to assess whether trees from different models or MCMC chains stem from
+the same underlying distribution.
+
+## Installation
+
+You can install the development version of epitree from
+[GitHub](https://github.com/) with:
+
+``` r
+# install.packages("pak")
+pak::pak("CyGei/epitree")
+```
+
+# Quick start
+
+``` r
+library(epitree)
+#> Registered S3 methods overwritten by 'adegraphics':
+#>   method         from
+#>   biplot.dudi    ade4
+#>   kplot.foucart  ade4
+#>   kplot.mcoa     ade4
+#>   kplot.mfa      ade4
+#>   kplot.pta      ade4
+#>   kplot.sepan    ade4
+#>   kplot.statis   ade4
+#>   scatter.coa    ade4
+#>   scatter.dudi   ade4
+#>   scatter.nipals ade4
+#>   scatter.pco    ade4
+#>   score.acm      ade4
+#>   score.mix      ade4
+#>   score.pca      ade4
+#>   screeplot.dudi ade4
+
+# Simulate two chains of transmission trees
+chainA <- lapply(1:100, function(i) {
+  make_tree(20, R = 2, stochastic = TRUE) |>
+    igraph::as_long_data_frame()
+})
+chainB <- lapply(1:100, function(i) {
+  make_tree(20, R = 4, stochastic = TRUE) |>
+    igraph::as_long_data_frame()
+})
+
+# Compare the two chains
+result <- compare_chains(chainA, chainB)
+#> Warning in att$heading[2] <- deparse(match.call(), width.cutoff = 500L): number
+#> of items to replace is not a multiple of replacement length
+print(result)
+#> Permutation test for adonis under reduced model
+#> Permutation: free
+#> Number of permutations: 999
+#> 
+#> (function (formula, data, permutations = 999, method = "bray", sqrt.dist = FALSE, add = FALSE, by = NULL, parallel = getOption("mc.cores"), na.action = na.fail, strata = NULL, ...) 
+#>           Df SumOfSqs      R2      F Pr(>F)    
+#> Model      1     7965 0.14791 34.371  0.001 ***
+#> Residual 198    45884 0.85209                  
+#> Total    199    53850 1.00000                  
+#> ---
+#> Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
+```
